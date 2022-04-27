99e11f922288df237a6cbb7570fbb3662c6a2c58
Arun S <101079723+arun-gits@users.noreply.github.com>
2022-04-27 10:03:32 +0530

    added src folder

---
+++ src/main/java/com/chocoapp/chocoappapi/ChocoappApiApplication.java
@@ -0,0 +1,13 @@
+package com.chocoapp.chocoappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChocoappApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChocoappApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chocoapp/chocoappapi/ChocoappApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chocoapp.chocoappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChocoappApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
